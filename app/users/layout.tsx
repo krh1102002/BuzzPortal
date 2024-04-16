f0a12a8d0cd5d45ad59308c9a91865b7d8018897
Kunal Hulke <hulke1102002@gmail.com>
2024-04-16 23:40:05 +0530

    adding the users list

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import Sidebar from "../components/sidebar/Sidebar";
+import getUsers from "../actions/getUsers";
+import UserList from "./components/UserList";
 
 export default async function userLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const users = await getUsers();
   return (
     <Sidebar>
-      <div className="h-full">{children}</div>
+      <div className="h-full">
+        <UserList items={users} />
+
+        {children}
+      </div>
     </Sidebar>
   );
 }
